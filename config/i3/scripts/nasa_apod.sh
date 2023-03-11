0a409f71b31bf08256ced80421b2fccece9fb059
Clément Jacquemaire <clement@unifai.fr>
2023-03-11 13:32:03 +0100

    :sparkles: ADD save wallpaper for beautiful nasa image

--- config/i3/scripts/nasa_apod.sh
+++ config/i3/scripts/nasa_apod.sh
@@ -5,20 +5,35 @@
 
 # Retrieve NASA_API_KEY
 source $HOME/Documents/projects/personal/dotfiles/.env
+
 # Set output destination
-WALLPAPER_PATH=$HOME/.wallpaper.jpg
+DEFAULT_EXTENSION=jpg
+WALLPAPER_PATH=$HOME/.wallpaper.$DEFAULT_EXTENSION
 
-APOD=$(curl --location --request GET https://api.nasa.gov/planetary/apod\?api_key\=${NASA_API_KEY})
-HD_CURL=$(echo $APOD | jq -r '.hdurl')
-if [[ -n HD_CURL ]]
-then
-    # Get HD day picture
-    curl $HD_CURL --output $WALLPAPER_PATH
-else
-    # fallback
-    URL=$(echo $APOD | jq -r '.url')
-    curl $URL --output $WALLPAPER_PATH
-fi
+function fetchWallpaper {
+    APOD=$(curl --location --request GET https://api.nasa.gov/planetary/apod\?api_key\=${NASA_API_KEY})
+    HD_CURL=$(echo $APOD | jq -r '.hdurl')
+    if [[ -n HD_CURL ]]
+    then
+        # Get HD day picture
+        curl $HD_CURL --output $WALLPAPER_PATH
+    else
+        # fallback
+        URL=$(echo $APOD | jq -r '.url')
+        curl $URL --output $WALLPAPER_PATH
+    fi
+    return $WALLPAPER_PATH
+}
 
-# # Set wallpaper
-feh --bg-fill $WALLPAPER_PATH
+function setWallpaper {
+    # Set wallpaper
+    feh --bg-fill ${1:-WALLPAPER_PATH}
+}
+
+function saveWallpaper {
+    BACKUP_PATH=$HOME/Pictures/NASA
+    PREFIX=NASA_APOD_
+    CURRENT_DATE=$(date +'%Y-%m-%d')
+    mkdir -p $BACKUP_PATH
+    cp $WALLPAPER_PATH $BACKUP_PATH/$PREFIX$CURRENT_DATE.$DEFAULT_EXTENSION
+}
